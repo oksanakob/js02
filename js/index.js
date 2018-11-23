f2c3cfaef785915a7296cdc3b4896e577b4533cb
Oksana <oksanakob21@gmail.com>
2018-11-23 17:13:10 +0000

    Some changes

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+'use strict';
+
 let teslaX = {};
 teslaX['model'] = 'X'; // Not recommended
 teslaX.make = 'Tesla'; // Recommended
@@ -18,3 +20,55 @@ let teslaS = {
 
 teslaX.beep();
 teslaS.beep();
+
+function Car(model, make, price) {
+    this.model = model;
+    this.make = make;
+    this.price = price;
+    return this;
+}
+Car.prototype.beep = function() {
+        document.write(this.make + ' ' 
+          + this.model + ' beep-beep<br>');
+}
+
+
+let chevyCorvette = new Car('Corvette', 'Chevrolet', 75000);
+let chevyTahoe = new Car('Tahoe', 'Chevrolet', 65000);
+chevyCorvette.beep();
+chevyTahoe.beep();
+Car.prototype.beep = function() {
+        document.write(this.model + ' ' 
+          + this.make + ' beep-beep<br>');
+}
+chevyCorvette.beep();
+chevyTahoe.beep = function() {document.write('Hello<br>')};
+chevyTahoe.beep();
+
+class SuperCar {
+    constructor(model, make, price) {
+        this.model = model;
+        this.make = make;
+        this.price = price;
+    }
+    beep() {
+        document.write('Supercar ' + this.model + ' '  + this.make + ' beep-beep<br>');
+    }
+}
+
+let bugattiVeyron = new SuperCar('Veyron', 'Bugatti', 1000000);
+bugattiVeyron.beep();
+let bugattiChiron = Object.create(bugattiVeyron);
+bugattiChiron.model = 'Chiron';
+bugattiChiron.beep();
+
+const jsonCar = `
+{
+    "make": "Skoda",
+    "model": "Octavia",
+    "price": 20000
+}`;
+
+let skodaOctavia = JSON.parse(jsonCar);
+skodaOctavia.beep = beep;
+skodaOctavia.beep();
